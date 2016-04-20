263aa93accc6f988e67bb9574596bfd7d332c6f8
florentinerb <florentin.erb@gibmit.ch>
2016-04-20 16:10:27 +0200

    GUIClasses added

---
+++ YallaPlayer/src/gui/GUIStarter.java
@@ -0,0 +1,87 @@
+package gui;
+
+import javafx.application.Application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class GUIStarter extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("YallaPlayer");
+
+        initRootLayout();
+
+       // showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/Users/Mac-Flo/git/YallaPlayer/src/gui/FirstView.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+    String path = Test.class.getResource("/Kalimba.mp3").toString();
+    Media media = new Media(path);
+    MediaPlayer mp = new MediaPlayer(media);
+    mp.play();
+    */
+
+
+
+
+    /**
+     Shows the person overview inside the root layout.
+     **/
+    /*
+    public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(GUIStarter.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    */
+
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
