98414685ebccf5be7dd463938ddba997c273f04f
Florentin Erb <florentin.erb@gibmit.ch>
2016-04-21 08:20:40 +0200

    Added other library

--- YallaPlayer/src/TestImplementations/YallaReader.java
+++ YallaPlayer/src/TestImplementations/YallaReader.java
@@ -1,38 +1,50 @@
 package TestImplementations;
 import java.io.File;
-import java.io.RandomAccessFile;
-
-import org.farng.mp3.id3.ID3v1_1;
+import com.beaglebuddy.mp3.MP3;
 
 public class YallaReader {
-	public static void main(String[] args){
-		RandomAccessFile sourceFile;
-		ID3v1_1 reader;
+	public static void main(String[] args) {
+		String path = "C:\\Temp\\TestBibliothek";
 		File[] listOfFiles;
 		File folder = null;
-		
+		String format = null;
+		int countElements = 0;
+
 		try {
-			folder = new File("/Users/Mac-Flo/Desktop/TestBibliothek");
+			folder = new File(path);
 			listOfFiles = folder.listFiles();
-			
+
 			for (File file : listOfFiles) {
-				if (file.isFile()) {
-					sourceFile = new RandomAccessFile(file.getAbsolutePath(), "rw");
+				int i = file.getAbsolutePath().lastIndexOf('.');
+				if (i > 0) {
+					format = file.getAbsolutePath().substring(i + 1);
+				}
+				if (file.isFile() && format.equals("mp3")) {
 					try {
-						reader = new ID3v1_1(sourceFile);
-						System.out.println(reader.getTitle() + " - " + reader.getArtist());
-					} catch (Exception e){
-						System.out.println("Probleme mit File!");
+						MP3 mp3 = new MP3(file.getAbsolutePath());
+						countElements++;
+						if (mp3.getBand() != null) {
+							System.out.println(countElements + " " + mp3.getTitle() + " - " + mp3.getBand());
+						} else {
+							System.out.println(countElements + " " + mp3.getTitle() + " - " + mp3.getID3v1Tag().getArtist());
+						}
+
+					} catch (Exception e) {
+						System.out.println("Probleme mit File " + file.getAbsolutePath() + "\n");
+						e.printStackTrace();
 					}
+				} else {
+					System.out.println("Keine Mp3-Datei! Dies war eine " + format + " Datei!");
 				}
 			}
-		} catch (Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Angegebener Pfad existiert nicht!");
 		} finally {
-			if(folder != null){
+			if (folder != null) {
 				folder.deleteOnExit();
 			}
 		}
+		
 	}
 }
